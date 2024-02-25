d13d32b07e8dccfae59df413e9c4e1e10e97d4a7
Charles <lijianqiang125@gmail.com>
2024-02-25 21:25:57 +0800

    选中的时区置顶，已经选择过的时区也排在前面

--- lib/timezone_select_widget.dart
+++ lib/timezone_select_widget.dart
@@ -3,6 +3,7 @@
 // found in the LICENSE file.
 
 import 'package:flutter/material.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 import 'package:time_picker_with_timezone/time_with_timezone.dart';
 import 'package:time_picker_with_timezone/timezone_util.dart';
 
@@ -52,22 +53,13 @@ class _TimeZoneSelectWidgetState extends State<TimeZoneSelectWidget> {
     super.dispose();
   }
 
-  @override
-  Widget build(BuildContext context) {
-    final List<Widget> timezoneListWidget = [];
+  Future<List<Widget>> _buildTimeZoneWidgetList() async {
+    final List<Widget> timeZoneListWidget = [];
+    final List<Widget> timeZoneHistoryListWidget = [];
 
-    //如果有已选中数据，就添加到列表首位
-    if (widget.initTimeZoneData != null) {
-      timezoneListWidget.add(
-        _buildTimeZoneItemWidget(
-          timeZoneData: widget.initTimeZoneData!,
-          selected: true,
-          onTap: (timeZoneData) {
-            widget.onTimeZoneSelected?.call(timeZoneData);
-          },
-        ),
-      );
-    }
+    //已选择过的历史数据
+    final SharedPreferences prefs = await SharedPreferences.getInstance();
+    final List<String> historyList = prefs.getStringList('selectedTimeZoneHistoryList') ?? [];
 
     if (widget.customTimeZoneDataList == null) {
       tz.timeZoneDatabase.locations.forEach((name, location) {
@@ -81,7 +73,25 @@ class _TimeZoneSelectWidgetState extends State<TimeZoneSelectWidget> {
           return;
         }
 
-        timezoneListWidget.add(
+        if (historyList.contains(name)) {
+          timeZoneHistoryListWidget.add(
+            _buildTimeZoneItemWidget(
+              timeZoneData: TimeZoneData(
+                name: name,
+                abbreviation: currentTimeZone.abbreviation,
+                offset: currentTimeZone.offset,
+                isDst: currentTimeZone.isDst,
+              ),
+              selected: name == widget.initTimeZoneData?.name,
+              onTap: (timeZoneData) {
+                _handleSelect(timeZoneData);
+              },
+            ),
+          );
+          return;
+        }
+
+        timeZoneListWidget.add(
           _buildTimeZoneItemWidget(
             timeZoneData: TimeZoneData(
               name: name,
@@ -91,7 +101,7 @@ class _TimeZoneSelectWidgetState extends State<TimeZoneSelectWidget> {
             ),
             selected: name == widget.initTimeZoneData?.name,
             onTap: (timeZoneData) {
-              widget.onTimeZoneSelected?.call(timeZoneData);
+              _handleSelect(timeZoneData);
             },
           ),
         );
@@ -106,27 +116,98 @@ class _TimeZoneSelectWidgetState extends State<TimeZoneSelectWidget> {
           continue;
         }
 
-        timezoneListWidget.add(
+        if (historyList.contains(element.name)) {
+          timeZoneHistoryListWidget.add(
+            _buildTimeZoneItemWidget(
+              timeZoneData: TimeZoneData(
+                name: element.name,
+                abbreviation: element.abbreviation,
+                offset: element.offset,
+                isDst: element.isDst,
+              ),
+              selected: element.name == widget.initTimeZoneData?.name,
+              onTap: (timeZoneData) {
+                _handleSelect(timeZoneData);
+              },
+            ),
+          );
+          continue;
+        }
+
+        timeZoneListWidget.add(
           _buildTimeZoneItemWidget(
             timeZoneData: element,
             selected: element.name == widget.initTimeZoneData?.name,
             onTap: (timeZoneData) {
-              widget.onTimeZoneSelected?.call(timeZoneData);
+              _handleSelect(timeZoneData);
             },
           ),
         );
       }
     }
 
+    if (widget.initTimeZoneData != null || historyList.isNotEmpty) {
+      timeZoneListWidget.insert(0, const Divider());
+    }
+
+    if (historyList.isNotEmpty) {
+      timeZoneListWidget.insertAll(0, timeZoneHistoryListWidget);
+    }
+
+    //如果有已选中数据，就添加到列表首位，无论搜索什么，都会显示在最上面
+    if (widget.initTimeZoneData != null) {
+      timeZoneListWidget.insert(
+        0,
+        _buildTimeZoneItemWidget(
+          timeZoneData: widget.initTimeZoneData!,
+          selected: true,
+          onTap: (timeZoneData) {
+            widget.onTimeZoneSelected?.call(timeZoneData);
+          },
+        ),
+      );
+    }
+
+    return timeZoneListWidget;
+  }
+
+  @override
+  Widget build(BuildContext context) {
     return SizedBox(
       height: MediaQuery.of(context).size.height / 2,
       width: 1000, //这样会被限制在最宽
-      child: Scrollbar(
-        child: ListView(
-          children: timezoneListWidget,
-        ),
+      child: FutureBuilder<List<Widget>>(
+        future: _buildTimeZoneWidgetList(),
+        builder: (BuildContext context, AsyncSnapshot<List<Widget>> snapshot) {
+          if (snapshot.connectionState == ConnectionState.waiting) {
+            return const Center(
+              child: CircularProgressIndicator(),
+            );
+          }
+          if (snapshot.hasError) {
+            return Center(
+              child: Text('Error: ${snapshot.error}'),
+            );
+          }
+          return Scrollbar(
+            child: ListView(
+              children: snapshot.data!,
+            ),
+          );
+        },
       ),
     );
+  }
+
+  //选中后返回并存储选中历史
+  void _handleSelect(TimeZoneData timeZoneData) async {
+    widget.onTimeZoneSelected?.call(timeZoneData);
+    final SharedPreferences prefs = await SharedPreferences.getInstance();
+    final List<String> items = prefs.getStringList('selectedTimeZoneHistoryList') ?? [];
+    if (!items.contains(timeZoneData.name)) {
+      items.add(timeZoneData.name);
+      await prefs.setStringList('selectedTimeZoneHistoryList', items);
+    }
   }
 
   //创建时区列表项
